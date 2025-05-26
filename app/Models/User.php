95490a3daa3074bc9d5e8093f729de434215526b
PepperFM <Damon3453@yandex.ru>
2025-05-26 16:28:21 +0300

    fix filament

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,15 +5,12 @@ declare(strict_types=1);
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Filament\Models\Contracts\FilamentUser;
-use Filament\Models\Contracts\HasName;
-use Filament\Panel;
 use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-class User extends Authenticatable implements FilamentUser, HasName
+class User extends Authenticatable
 {
     use HasFactory, Notifiable;
 
@@ -45,17 +42,5 @@ class User extends Authenticatable implements FilamentUser, HasName
         return Attribute::make(
             set: static fn(string $value) => strtolower($value),
         );
-    }
-
-    public function getFilamentName(): string
-    {
-        return $this->email;
-    }
-
-    public function canAccessPanel(Panel $panel): bool
-    {
-        // todo:
-
-        return true;
     }
 }
